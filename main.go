a141a4de8f04f21d297669b772d57dc80b95c91c
havok <alireza.karampoor@gmail.com>
2024-10-18 09:40:00 +0330

    begining to undrestand the api

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -9,38 +10,42 @@ import (
 )
 
 func main() {
-	// Get address from environment variable set by Nvim.
-	addr := os.Getenv("NVIM_LISTEN_ADDRESS")
-	if addr == "" {
-		addr = "127.0.0.1:8888"
+	// Start Neovim in headless mode
+	v, err := nvim.NewChildProcess(nvim.ChildProcessArgs("--embed"))
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	v, err := nvim.Dial(addr)
-	// Dial with default options.
+	apis, err := v.APIInfo()
 	if err != nil {
 		panic(err)
 	}
+	apiFile, err := os.Create("api-info.json")
+	if err != nil {
+		panic(err)
+	}
+	js, err := json.Marshal(apis)
+	if err != nil {
+		panic(err)
+	}
+	apiFile.WriteString(string(js))
 
-	// Cleanup on return.
-	defer v.Close()
-
-	bufs, err := v.Buffers()
+	v.Subscribe("redraw")
+	v.RegisterHandler("redraw", func(args []any) {
+		if args[0] == "grid_line" {
+			fmt.Println(args[1])
+		}
+	})
+	// Attach the UI
+	err = v.AttachUI(80, 24, map[string]interface{}{
+		"ext_linegrid": true,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Get the names using a single atomic call to Nvim.
-	names := make([]string, len(bufs))
-	b := v.NewBatch()
-	for i, buf := range bufs {
-		b.BufferName(buf, &names[i])
-	}
-	if err := b.Execute(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Print the names.
-	for _, name := range names {
-		fmt.Println(name)
+	err = v.Serve()
+	if err != nil {
+		panic(err)
 	}
 }
